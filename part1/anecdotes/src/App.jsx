2c5fe4439a46e756f6cfca17973fb04aac6535cd
CascoOnce <nick.carrascovillegas@hotmail.com>
2025-07-31 13:57:16 -0500

    1.13

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ];
-  const [votes, setVotes] =  useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [votes, setVotes] =  useState(new Uint8Array(anecdotes.length));
   const [selected, setSelected] = useState(0);
   
   const randomNumber = (max, min) => {
