fddcf4f6678ff2c680a590e58bd1605857f9a2d1
CascoOnce <nick.carrascovillegas@hotmail.com>
2025-08-13 21:19:42 -0500

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,44 +2,43 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
+  // INPUT
   const handleNameChange = (event) => {
     console.log(event.target.value); // Muestra en consola el valor del input
     setNewName(event.target.value); // Setea el valor del input
   };
+  const handleNumberChange = (event) => {
+    console.log(event.target.value); // Muestra en consola el valor del input
+    setNewNumber(event.target.value); // Setea el valor del input
+  };
 
-  const addName = (event) => {
+  // BOTON
+  const addNumbers = (event) => {
     event.preventDefault(); // Evitar que se recargue la pagina
     // Nueva persona que se agrega
     const newPerson = {
       name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     };
-    // existInList(1, persons);
-    if(!isInList(newPerson, persons)){
+    if(isInList(newPerson, persons)){
+      alert(`${newPerson.name} is already added to phonebook`); // PLANILLA DE CADENAS
+    }else{
       console.log('button clicked'); // Informar en consola
       setPersons(persons.concat(newPerson)); // Se agrega al arreglo
       setNewName(''); // Para que el input empiece de nuevo
-    }else{
-      // PLANILLA DE CADENAS
-      alert(`${newPerson.name} is already added to phonebook`);
+      setNewNumber(''); // Para que el input empiece de nuevo
     }
-    
   }
 
   const listNames = persons.map(name => 
-    <div>{name.name}</div>
+    <div key={name.id} >{name.name} {name.number}</div>
   )
-
-  // const isInList = (value, list) => {
-  //   const values = list.filter((element) => {
-  //     return element.name == value.name;
-  //   })
-  //   if(values.length > 0) return true;
-  //   else return false;
-  // }
 
   const isInList = (value, list) => list.some(element =>
     element.name === value.name
@@ -53,7 +52,10 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit" onClick={addName} >
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addNumbers} >
             add
           </button>
         </div>
