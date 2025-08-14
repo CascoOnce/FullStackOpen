7217b04cb4fbec9c117cbd316131687ea4092c0f
CascoOnce <nick.carrascovillegas@hotmail.com>
2025-08-13 21:20:42 -0500

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,9 +3,13 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filterPerson, setFilterPerson] = useState('');
 
   // INPUT
   const handleNameChange = (event) => {
@@ -16,7 +20,10 @@ const App = () => {
     console.log(event.target.value); // Muestra en consola el valor del input
     setNewNumber(event.target.value); // Setea el valor del input
   };
-
+  const handleFilterChange = (event) => {
+    console.log(event.target.value); // Muestra en consola el valor del input
+    setFilterPerson(event.target.value); // Setea el valor del input
+  };
   // BOTON
   const addNumbers = (event) => {
     event.preventDefault(); // Evitar que se recargue la pagina
@@ -35,18 +42,28 @@ const App = () => {
       setNewNumber(''); // Para que el input empiece de nuevo
     }
   }
-
-  const listNames = persons.map(name => 
+  // LIST
+  const phonesToShsow = persons.filter(element => {
+    const lower = element.name.toLowerCase();
+    // console.log(lower);
+    return lower.includes(filterPerson.toLowerCase());
+  })
+  const listNames = phonesToShsow.map(name => 
     <div key={name.id} >{name.name} {name.number}</div>
   )
-
+  // VALIDACION REPETIDO
   const isInList = (value, list) => list.some(element =>
     element.name === value.name
   );
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter show with <input value={filterPerson} onChange={handleFilterChange} />
+        </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
