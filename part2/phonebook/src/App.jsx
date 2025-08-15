68f31a62e3b686bccb78b21688041ef4806baa1f
CascoOnce <nick.carrascovillegas@hotmail.com>
2025-08-15 14:12:10 -0500

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({value, change}) => {
   return (
@@ -37,15 +38,23 @@ const Persons = ({list}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterPerson, setFilterPerson] = useState('');
+
+  // SERVER CALL
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3000/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        console.log(response.data);
+        setPersons(response.data);
+      })
+  }, []);
+  console.log('render', persons.length, 'notes');
 
   // INPUT
   const handleNameChange = (event) => {
