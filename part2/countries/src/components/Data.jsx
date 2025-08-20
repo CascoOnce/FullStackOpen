eeb467dd844a692dd68b875b0f18af498e15d268
CascoOnce <nick.carrascovillegas@hotmail.com>
2025-08-20 17:39:12 -0500

    2.20

--- part2/countries/src/components/Data.jsx
+++ part2/countries/src/components/Data.jsx
@@ -1,20 +1,57 @@
-const Country = ({data}) => {
-  const languages = Object.entries(data.languages).map(([key, value]) => 
-    <li key={key} >{value}</li>
-  );
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-  return (
-    <div>
-      <h1>{data.name.common}</h1>
-      <div>Capital {data.capital}</div>
-      <div>Area {data.area}</div>
-      <h2>Languages</h2>
-      <ul>
-        {languages}
-      </ul>
-      <img src={data.flags.png} alt="" />
-    </div>
+const Country = ({data}) => {
+  // CONTROL DE VARIABLES
+  const [weather, setWeather] = useState(null); // TENDRA TODA LA INFO DEL CLIMA
+  const languages = Object.entries(data.languages).map(([key, value]) => 
+    <li key={key} >{value}</li> // GUARDA LOS LENGUAJES DEL PAIS
   );
+  const api_cloud_key = import.meta.env.VITE_CLOUD_API_KEY;
+  const link = `https://api.openweathermap.org/data/3.0/onecall?lat=${data.capitalInfo.latlng[0]}&lon=${data.capitalInfo.latlng[1]}&appid=${api_cloud_key}`;
+  // console.log(link);
+  // LLAMADA A LA API DEL CLIMA
+  useEffect(() => {
+    axios
+      .get(link)
+      .then(response => {
+        setWeather(response.data);
+      })
+  }, []);
+
+  if(weather === null){
+    return (
+      <div>
+        <h1>{data.name.common}</h1>
+        <div>Capital {data.capital}</div>
+        <div>Area {data.area}</div>
+        <h2>Languages</h2>
+        <ul>
+          {languages}
+        </ul>
+        <img src={data.flags.png} alt="" />
+        <h2>Weather in {data.capital}</h2>
+      </div>
+    );
+  }else{
+    return (
+      <div>
+        <h1>{data.name.common}</h1>
+        <div>Capital {data.capital}</div>
+        <div>Area {data.area}</div>
+        <h2>Languages</h2>
+        <ul>
+          {languages}
+        </ul>
+        <img src={data.flags.png} alt="" />
+        <h2>Weather in {data.capital}</h2>
+        <div>Temperature {Number(weather.current.temp - 273.15).toFixed(2)}</div>
+        <img src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`} alt="" />
+        <div>Wind {weather.current.wind_speed} m/s</div>
+      </div>
+    );
+  }
+  
 }
 
 const Data = ({countriesToShow, setFilter}) => {
